c1f9792e0fe569e4ebd59ad4dce9909fc1a9b653
LamineBarghouda <laminebarghouda@gmail.com>
2019-07-28 22:18:01 +0100

    almost there

--- Components/FilmDetail.js
+++ Components/FilmDetail.js
@@ -12,11 +12,22 @@ class FilmDetail extends React.Component {
         super(props)
         this.state = {
             film: undefined,
-            isLoading: true
+            isLoading: false
         }
     }
 
     componentDidMount() {
+        const favoriteFilmIndex = this.props.favoritesFilm.findIndex(item => item.id === this.props.navigation.state.params.idFilm)
+        if (favoriteFilmIndex !== -1) { // Film déjà dans nos favoris, on a déjà son détail
+            // Pas besoin d'appeler l'API ici, on ajoute le détail stocké dans notre state global au state de notre component
+            this.setState({
+                film: this.props.favoritesFilm[favoriteFilmIndex]
+            })
+            return
+        }
+        // Le film n'est pas dans nos favoris, on n'a pas son détail
+        // On appelle l'API pour récupérer son détail
+        this.setState({ isLoading: true })
         getFilmDetailFromApi(this.props.navigation.state.params.idFilm).then(data => {
             this.setState({
                 film: data,
@@ -25,58 +36,19 @@ class FilmDetail extends React.Component {
         })
     }
 
-    componentDidUpdate() {
-        console.log("componentDidUpdate : ")
-        console.log(this.props.favoritesFilm)
-    }
-
     _displayLoading() {
         if (this.state.isLoading) {
             return (
                 <View style={styles.loading_container}>
-                    <ActivityIndicator size='large'/>
+                    <ActivityIndicator size='large' />
                 </View>
             )
         }
     }
 
     _toggleFavorite() {
-        const action = {type: "TOGGLE_FAVORITE", value: this.state.film}
+        const action = { type: "TOGGLE_FAVORITE", value: this.state.film }
         this.props.dispatch(action)
-    }
-
-    _displayFilm() {
-        const {film} = this.state
-        if (film != undefined) {
-            return (
-                <ScrollView style={styles.scrollview_container}>
-                    <Image
-                        style={styles.image}
-                        source={{uri: getImageFromApi(film.backdrop_path)}}
-                    />
-                    <Text style={styles.title_text}>{film.title}</Text>
-                    <TouchableOpacity
-                        style={styles.favorite_container}
-                        onPress={() => this._toggleFavorite()}>
-                        {this._displayFavoriteImage()}
-                    </TouchableOpacity>
-                    <Text style={styles.description_text}>{film.overview}</Text>
-                    <Text style={styles.default_text}>Sorti
-                        le {moment(new Date(film.release_date)).format('DD/MM/YYYY')}</Text>
-                    <Text style={styles.default_text}>Note : {film.vote_average} / 10</Text>
-                    <Text style={styles.default_text}>Nombre de votes : {film.vote_count}</Text>
-                    <Text style={styles.default_text}>Budget : {numeral(film.budget).format('0,0[.]00 $')}</Text>
-                    <Text style={styles.default_text}>Genre(s) : {film.genres.map(function (genre) {
-                        return genre.name;
-                    }).join(" / ")}
-                    </Text>
-                    <Text style={styles.default_text}>Companie(s) : {film.production_companies.map(function (company) {
-                        return company.name;
-                    }).join(" / ")}
-                    </Text>
-                </ScrollView>
-            )
-        }
     }
 
     _displayFavoriteImage() {
@@ -93,6 +65,39 @@ class FilmDetail extends React.Component {
         )
     }
 
+    _displayFilm() {
+        const { film } = this.state
+        if (film != undefined) {
+            return (
+                <ScrollView style={styles.scrollview_container}>
+                    <Image
+                        style={styles.image}
+                        source={{uri: getImageFromApi(film.backdrop_path)}}
+                    />
+                    <Text style={styles.title_text}>{film.title}</Text>
+                    <TouchableOpacity
+                        style={styles.favorite_container}
+                        onPress={() => this._toggleFavorite()}>
+                        {this._displayFavoriteImage()}
+                    </TouchableOpacity>
+                    <Text style={styles.description_text}>{film.overview}</Text>
+                    <Text style={styles.default_text}>Sorti le {moment(new Date(film.release_date)).format('DD/MM/YYYY')}</Text>
+                    <Text style={styles.default_text}>Note : {film.vote_average} / 10</Text>
+                    <Text style={styles.default_text}>Nombre de votes : {film.vote_count}</Text>
+                    <Text style={styles.default_text}>Budget : {numeral(film.budget).format('0,0[.]00 $')}</Text>
+                    <Text style={styles.default_text}>Genre(s) : {film.genres.map(function(genre){
+                        return genre.name;
+                    }).join(" / ")}
+                    </Text>
+                    <Text style={styles.default_text}>Companie(s) : {film.production_companies.map(function(company){
+                        return company.name;
+                    }).join(" / ")}
+                    </Text>
+                </ScrollView>
+            )
+        }
+    }
+
     render() {
         return (
             <View style={styles.main_container}>
@@ -102,7 +107,6 @@ class FilmDetail extends React.Component {
         )
     }
 }
-
 
 const styles = StyleSheet.create({
     main_container: {
@@ -136,6 +140,9 @@ const styles = StyleSheet.create({
         color: '#000000',
         textAlign: 'center'
     },
+    favorite_container: {
+        alignItems: 'center',
+    },
     description_text: {
         fontStyle: 'italic',
         color: '#666666',
@@ -146,9 +153,6 @@ const styles = StyleSheet.create({
         marginLeft: 5,
         marginRight: 5,
         marginTop: 5,
-    },
-    favorite_container: {
-        alignItems: 'center', // Alignement des components enfants sur l'axe secondaire, X ici
     },
     favorite_image: {
         width: 40,
