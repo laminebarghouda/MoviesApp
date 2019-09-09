41f4c11d967436018f7d3fb643e1a528a5da13be
laminebarghouda <laminebarghouda@gmail.com>
2019-09-09 07:46:50 +0100

    Soon

--- Components/Test.js
+++ Components/Test.js
@@ -1,14 +1,38 @@
 // Components/Test.js
 
 import React from 'react'
-import { StyleSheet, View } from 'react-native'
+import { StyleSheet, View, Animated, Easing, PanResponder, Dimensions } from 'react-native'
 
 class Test extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            topPosition: 0,
+            leftPosition: 0,
+        }
+
+        var {height, width} = Dimensions.get('window');
+        this.panResponder = PanResponder.create({
+            onStartShouldSetPanResponder: (evt, gestureState) => true,
+            onPanResponderMove: (evt, gestureState) => {
+                let touches = evt.nativeEvent.touches;
+                if (touches.length == 1) {
+                    this.setState({
+                        topPosition: touches[0].pageY - height/2,
+                        leftPosition: touches[0].pageX - width/2
+                    })
+                }
+            }
+        })
+    }
 
     render() {
         return (
             <View style={styles.main_container}>
-                <View style={styles.subview_container}>
+                <View
+                    {...this.panResponder.panHandlers}
+                    style={[styles.animation_view, { top: this.state.topPosition, left: this.state.leftPosition }]}>
                 </View>
             </View>
         )
@@ -21,8 +45,10 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center'
     },
-    subview_container: {
-
+    animation_view: {
+        backgroundColor: 'red',
+        width: 100,
+        height: 100
     }
 })
 
--- Navigation/Navigation.js
+++ Navigation/Navigation.js
@@ -6,6 +6,7 @@ import { createStackNavigator, createBottomTabNavigator, createAppContainer } fr
 import Search from '../Components/Search'
 import FilmDetail from '../Components/FilmDetail'
 import Favorites from '../Components/Favorites'
+import Test from '../Components/Test'
 
 const SearchStackNavigator = createStackNavigator({
     Search: {
